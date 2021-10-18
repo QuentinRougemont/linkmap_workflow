21f6731b2b1d461da3c0055c15b934ae9a8df24d
Quentin <quentinrougemont@orange.fr>
2021-10-18 16:16:30 +0200

    edited scripts

--- 00_scripts/06.order_marker.sh
+++ 00_scripts/06.order_marker.sh
@@ -13,41 +13,42 @@ rm 10.ordered_evaluated."$base_map" 2>/dev/null
 mkdir -p 10.ordered_"$base_map"/
 
 for LG in $(cat chromolist ) ; do
-	echo "order markers for LG $LG"
-	zcat $data |\
-		java -cp ~/work/softwares/lep-map3/bin/ OrderMarkers2 \
-		map=$map \
-		numThreads=5 \
-			recombination2=0 \
-			chromosome=$LG \
-			numMergeIterations=6 \
-			outputPhasedData=1 \
-               data=- >10.ordered_"$base_map"/order_LG"$LG".txt 2>10.ordered_"$base_map"/stdout_LG"$LG".txt  
-	     
-		infile=10.ordered_"$base_map"/order_LG"$LG".txt
-         
-		#mkdir 10.ordered_evaluated_"$map"
+    echo "order markers for LG $LG"
+    zcat $data |\
+        java -cp ~/work/softwares/lep-map3/bin/ OrderMarkers2 \
+        map=$map \
+        numThreads=5 \
+            recombination2=0 \
+            chromosome=$LG \
+            numMergeIterations=6 \
+            outputPhasedData=1 \
+            data=- >10.ordered_"$base_map"/order_LG"$LG".txt 2>10.ordered_"$base_map"/stdout_LG"$LG".txt  
+        
+        infile=10.ordered_"$base_map"/order_LG"$LG".txt
+        
+        #evaluate order:
+        for rep in $(seq 5 ) ; 
+        echo "evaluate order for LG $LG and rep $rep"
+        do
+            mkdir -p 10.ordered_evaluated_"$base_map"/"$rep"
 
-		for rep in $(seq 5 ) ; 
-		echo "evaluate order for LG $LG and rep $rep"
-		do
-		mkdir -p 10.ordered_evaluated_"$base_map"/"$rep"
-
-			zcat $data |\
-			java -cp ~/work/softwares/lep-map3/bin/ OrderMarkers2 \
-			evaluateOrder=$infile \
-			numThreads=5 \
-			data=- \
-			numMergeIterations=6 \
-			recombination2=0 \
-			chromosome=$LG \
-			outputPhasedData=1 >10.ordered_evaluated_"$base_map"/"$rep"/order_evaluated_LG"$LG".txt 2>10.ordered_evaluated_"$base_map"/"$rep"/stdout_LG"$LG".txt
-		done
+            zcat $data |\
+            java -cp ~/work/softwares/lep-map3/bin/ OrderMarkers2 \
+            evaluateOrder=$infile \
+            numThreads=5 \
+            data=- \
+            numMergeIterations=6 \
+            recombination2=0 \
+            chromosome=$LG \
+            outputPhasedData=1 >10.ordered_evaluated_"$base_map"/"$rep"/order_evaluated_LG"$LG".txt 2>10.ordered_evaluated_"$base_map"/"$rep"/stdout_LG"$LG".txt
+        done
 done ;
 
 zcat $data | cut -f 1,2 | awk '(NR>=7)' >snps.txt
 exit
-#search the best replicate before running this line:
+
+#search the best replicate (lower likelihhood in each of the repetition) before running this line:
 #note that first line of snps.txt contains "CHR POS"
+#order.txt should be renmae to match the best map for each LG
 awk -vFS="\t" -vOFS="\t" '(NR==FNR){s[NR-1]=$0}(NR!=FNR){if ($1 in s) $1=s[$1];print}' snps.txt order.txt >order.mapped
 #because of first line of snps.txt, we use NR-1 instead of NR
